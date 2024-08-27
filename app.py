7f2a86493e9223ac01474f338708985687254763
suganeswar <suganeswar2000@gmail.com>
2024-08-27 14:05:35 +0530

    updatedd

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ Pictured = Image.open("images/Pictured.png")
 with st.container():
     st.subheader("Hi, Iam Suganeswar Savadamuthu :wave:")
     st.title("Software Developer")
-    st.write("Masters Graduate @ Pace University,Newyork| AWS Certified Developer – Associate.")
+    st.write("Masters Graduate @ Pace University,Newyork.")
     st.write("Gmail: suganeswar2000@gmail.com")
     st.write("Mobile: +91 9942645560")
     st.write("[Linkedin >]https://www.linkedin.com/in/suganeswar-savadamuthu-069097153")
